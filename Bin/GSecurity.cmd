8578ef4c778caf412bf4290fd92c906cf1c1ba13
GorstaksWindows <161788499+GorstaksWindows@users.noreply.github.com>
2024-04-05 06:07:43 +0200

    Update GSecurity.cmd

--- Bin/GSecurity.cmd
+++ Bin/GSecurity.cmd
@@ -58,6 +58,4 @@ rd /s /q "%windir%\System32\Group Policy"
 rd /s /q "%windir%\System32\Group Policy Users"
 rd /s /q "%windir%\SysWOW64\Group Policy"
 rd /s /q "%windir%\SysWOW64\Group Policy Users"
-Reg delete "HKLM\SOFTWARE\Policies" /f
-Reg delete "HKCU\Software\Policies" /f
 lgpo /g .\
