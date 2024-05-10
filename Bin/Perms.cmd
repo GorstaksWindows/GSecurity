e61de8b870f21e5af1af5957b6fb87d37da184bb
GorstaksWindows <161788499+GorstaksWindows@users.noreply.github.com>
2024-05-10 07:01:41 +0200

    Add files via upload

--- Bin/Perms.cmd
+++ Bin/Perms.cmd
@@ -1,14 +1,12 @@
-@echo off
-for %%d in (A: B: C: D: E: F: G: H: I: J: K: L: M: N: O: P: Q: R: S: T: U: V: W: X: Y: Z:) do (
-    icacls %%d\ /remove "Administrators"
-    icacls %%d\ /grant Administrators:(OI)(CI)(RX)
-    icacls %%d\ /grant *S-1-2-1:(OI)(CI)(M) /T /C
-    icacls %%d\ /remove "System"
-    icacls %%d\ /remove "Users"
-    icacls %%d\ /remove "Authenticated Users"
+:: Perms
+for %%d in (A B C D E F G H I J K L M N O P Q R S T U V W X Y Z) do (
+    takeown /F %%d:\
+    icacls %%d:\ /remove "Administrators"
+    icacls %%d:\ /grant "Administrators":RX
+    icacls %%d:\ /remove "Authenticated Users"
+    icacls %%d:\ /remove "Users"
+    icacls %%d:\ /remove "System"
+    icacls %%d:\ /grant "*S-1-2-1":M
+    icacls %%d:\ /deny "Network":F
 )
-icacls C:\ /grant System:(OI)(CI)(F) /T /C
-set "folderPath=C:\Users\%username%"
-takeown /F %folderPath% /R /D Y
-icacls %folderPath% /inheritance:r
-icacls %folderPath% /grant %username%:(F) /T /C
+    icacls C:\ /grant "System":M
