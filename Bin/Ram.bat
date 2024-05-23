39b253adf4fc39600e7d45277c43597420359312
Goran Štambuk <161788499+GorstaksWindows@users.noreply.github.com>
2024-05-23 01:20:16 -0700

    Add files via upload

--- Bin/Ram.bat
+++ Bin/Ram.bat
@@ -2,6 +2,18 @@
 pushd %~dp0
 
 :Cleaner
+sc config edgeupdate start=disabled >nul 2>&1
+sc config edgeupdatem start=disabled >nul 2>&1
+sc config gupdate start=disabled >nul 2>&1
+sc config gupdatem start=disabled >nul 2>&1
+sc config MozillaMaintenance start=disabled >nul 2>&1
+sc stop AppXSvc >nul 2>&1
+sc stop BITS >nul 2>&1
+sc stop ClipSvc >nul 2>&1
+sc stop StorSvc >nul 2>&1
+sc stop Wcmsvc >nul 2>&1
+devicecleanup * -s -m:7d >nul 2>&1
+set /a CPU=%NUMBER_OF_CORES%*%MAX_CLOCK_SPEED%
 emptystandbylist.exe workingsets
 emptystandbylist.exe modifiedpagelist
 for /f "tokens=*" %%A in ('reg query "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\NetworkList\Profiles" /s /f "ProfileName" /k ^| findstr /i /c:"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\NetworkList\Profiles"') do (
---
+++ Bin/RunOnceEx.cmd
@@ -0,0 +1,22 @@
+%windir%\system32\bcdedit /set disabledynamictick yes >nul 2>&1
+%windir%\system32\bcdedit /set useplatformtick yes >nul 2>&1
+if "%BUILD%"=="Microsoft Windows [Version 10.0.19042.631]" ( %windir%\system32\bcdedit /set nx AlwaysOff >nul 2>&1 ) else (
+	reg add "HKLM\System\CurrentControlSet\Control\FeatureManagement\Overrides\4\2674077835" /v "EnabledState" /t REG_DWORD /d "1" /f >nul 2>&1
+	reg add "HKLM\System\CurrentControlSet\Control\FeatureManagement\Overrides\4\2674077835" /v "EnabledStateOptions" /t REG_DWORD /d "1" /f >nul 2>&1
+	reg add "HKLM\System\CurrentControlSet\Control\FeatureManagement\Overrides\4\2674077835" /v "Variant" /t REG_DWORD /d "0" /f >nul 2>&1
+	reg add "HKLM\System\CurrentControlSet\Control\FeatureManagement\Overrides\4\2674077835" /v "VariantPayload" /t REG_DWORD /d "0" /f >nul 2>&1
+	reg add "HKLM\System\CurrentControlSet\Control\FeatureManagement\Overrides\4\2674077835" /v "VariantPayloadKind" /t REG_DWORD /d "0" /f >nul 2>&1
+	reg add "HKLM\System\CurrentControlSet\Control\FeatureManagement\Overrides\4\4095660171" /v "EnabledState" /t REG_DWORD /d "1" /f >nul 2>&1
+	reg add "HKLM\System\CurrentControlSet\Control\FeatureManagement\Overrides\4\4095660171" /v "EnabledStateOptions" /t REG_DWORD /d "1" /f >nul 2>&1
+	reg add "HKLM\System\CurrentControlSet\Control\FeatureManagement\Overrides\4\4095660171" /v "Variant" /t REG_DWORD /d "0" /f >nul 2>&1
+	reg add "HKLM\System\CurrentControlSet\Control\FeatureManagement\Overrides\4\4095660171" /v "VariantPayload" /t REG_DWORD /d "0" /f >nul 2>&1
+	reg add "HKLM\System\CurrentControlSet\Control\FeatureManagement\Overrides\4\4095660171" /v "VariantPayloadKind" /t REG_DWORD /d "0" /f >nul 2>&1
+)
+devcon disable "=CDROM" >nul 2>&1
+devcon disable "=Printer" >nul 2>&1
+devcon disable "=PrintQueue" >nul 2>&1
+devcon disable ACPI\HP* >nul 2>&1
+devcon disable ACPI\MSFT01* >nul 2>&1
+lodctr /r >nul 2>&1
+lodctr /r >nul 2>&1
+
