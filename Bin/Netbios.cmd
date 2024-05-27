73df49fa6cd0d740e80429d8923810b121821abd
Goran Štambuk <161788499+GorstaksWindows@users.noreply.github.com>
2024-05-27 12:22:16 +0200

    Delete Bin directory

--- Bin/Netbios.cmd
+++
@@ -1,2 +0,0 @@
-:: Disable Netbios
-@powershell.exe -ExecutionPolicy Bypass -Command "Get-WmiObject -Class Win32_NetworkAdapterConfiguration | Where-Object { $_.IPEnabled -eq $true } | ForEach-Object { $_.SetTcpipNetbios(2) }"
--- Bin/System.cmd
+++
@@ -1,5 +0,0 @@
-:: System failure watch off
-wmic recoveros set WriteToSystemLog = False
-wmic recoveros set SendAdminAlert = False
-wmic recoveros set AutoReboot = False
-wmic recoveros set DebugInfoType = 0
