0a4fe79870ec78277b9e91c9b00674761b634fe2
Goran Štambuk <161788499+GorstaksWindows@users.noreply.github.com>
2024-07-04 12:57:09 +0200

    Add files via upload

---
+++ Bin/Ram.cmd
@@ -0,0 +1,9 @@
+:: Ram
+md "%systemdrive%\Windows\Cleaner"
+copy /y EmptyStandbyList.exe "%systemdrive%\Windows\Cleaner\"
+copy /y Ram.bat "%systemdrive%\Windows\Cleaner\"
+copy /y devicecleanup.exe "%systemdrive%\Windows\Cleaner\"
+schtasks /create /xml "Cleaner.xml" /tn "Cleaner" /ru ""
+for /f "tokens=2 delims==" %%s in ('wmic os get TotalVisibleMemorySize /format:value ^| findstr "TotalVisibleMemorySize"') do set "TotalVisibleMemorySize=%%s"
+set /a RAM=%TotalVisibleMemorySize%+1024000
+setx /m SVCHOSTSPLIT %RAM%
---
+++ Bin/User.cmd
@@ -0,0 +1,3 @@
+:: Default user
+net user defaultuser1 /delete
+net user defaultuser100000 /delete
