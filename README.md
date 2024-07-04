24c4ed0d9c918395dae320d6e122a78a86a9083d
Goran Štambuk <161788499+GorstaksWindows@users.noreply.github.com>
2024-07-04 22:00:02 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # GShield
 This script is meant for cyber victims, children, advanced home users or distro hoppers-testers. 
 It features extreme security and as such isn't meant for daily use.
-If you don't fall under these categories, and you aren't being bullied, do not run this script!
+If you don't fall under these categories, do not run this script!
 If you do, you do so at your own risk. I am not responsible for any damage caused.
+For security reasons, script should only be ran once and there is no uninstall.
---
+++ SetupComplete.cmd
@@ -0,0 +1,43 @@
+@echo off
+title GSecurity && color 0b
+
+:: Elevate
+>nul 2>&1 fsutil dirty query %systemdrive% || (
+    echo CreateObject^("Shell.Application"^).ShellExecute "%~0", "ELEVATED", "", "runas", 1 > "%temp%\uac.vbs"
+    "%temp%\uac.vbs"
+    exit /b
+)
+DEL /F /Q "%temp%\uac.vbs"
+
+:: Active Folder
+pushd %~dp0
+cd Bin
+setlocal enabledelayedexpansion
+
+:: Create a system restore point
+wmic.exe /Namespace:\\root\default Path SystemRestore Call CreateRestorePoint "Pre-GSecurity Restore Point", 100, 7
+
+:: Batch files execution
+for %%A in (*.cmd) do (
+    call "%%A"
+)
+
+:: PowerShell scripts execution
+for %%B in (*.ps1) do (
+    @powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%%B" -WindowStyle Hidden
+)
+
+:: Registry files import
+for %%C in (*.reg) do (
+    reg import "%%C"
+)
+
+:: MSI installation
+for %%D in (*.msi) do (
+    start /wait msiexec /i "%%D" /qn
+)
+
+:: Set Execution Policy for PowerShell
+powershell Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Bypass
+
+endlocal
